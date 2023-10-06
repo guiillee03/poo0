0cff89d6cc52683190c7913160683c239550fdc7
A6E22\manana <guillermoaalonsoo@gmail.com>
2023-10-06 17:08:20 +0200

    Cambiado el Sysetm.out.println();

--- poo0/src/poo0/Poo0.java
+++ poo0/src/poo0/Poo0.java
@@ -14,7 +14,7 @@ public class Poo0 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("KLK puñeta");
     }
     
 }
